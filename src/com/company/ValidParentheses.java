9cf9321724e8916492d228c2a669f0125749424b
Viktor Tsybulko <viktortsybulko2002@gmail.com>
2022-01-25 15:35:57 +0200

    Add problem about Valid Parentheses (20th problem on the leetcode)

---
+++ src/com/company/ValidParentheses.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Stack;
+
+/*
+* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+* determine if the input string is valid.
+*
+* An input string is valid if:
+*
+* 1)Open brackets must be closed by the same type of brackets.
+* 2)Open brackets must be closed in the correct order.
+* */
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        ValidParentheses vp = new ValidParentheses();
+        System.out.println(vp.isValid("([]))"));
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for(char ch : s.toCharArray()) {
+            if(isOpenParenthesis(ch)) {
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char popped = stack.pop();
+                if(!isParenthesisMatch(popped, ch)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean isOpenParenthesis(char ch) {
+        return ch == '(' || ch == '{' || ch == '[';
+    }
+
+    private boolean isParenthesisMatch(char open, char close) {
+        return (open=='(' && close==')')
+                || (open=='{' && close=='}')
+                || (open=='[' && close==']');
+    }
+}
